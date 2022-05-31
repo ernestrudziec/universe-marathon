323e67ec109a9b43f7037c2eb3c68629c9976107
ernestrudziec <ernest.rudziec@gmail.com>
2022-05-31 11:58:00 +0200

    feat: mapped data v1

---
+++ src/data/types.ts
@@ -0,0 +1,79 @@
+export type Genre =
+  | "mcuFilm"
+  | "nonMcuFilm"
+  | "nonMcuStronglyRecommendedFilm"
+  | "nonMcuRecommendedFilm"
+  | "oneshot"
+  | "mcuCanonShow"
+  | "netflixShow"
+  | "disneyPlusShow"
+  | "amazonPrimeShow";
+
+export type ItemId = string;
+type ItemIds = Array<ItemId> | null;
+
+export type GroupId = string;
+export type GroupIds = Array<GroupId> | null;
+
+export interface Collection {
+  items: ItemIds;
+  groups: GroupIds;
+}
+
+export interface Superset {
+  superfan: Collection | null;
+  casual: Collection | null;
+  speedrun: Collection | null;
+}
+
+export type Tiers = 1 | 2 | 3 | 4 | 5;
+
+export interface Item {
+  title: string;
+  genre: Genre;
+  id: ItemId;
+  //ids start with "f" - for films, "s" - for shows, "o" - for oneshots and webisodes
+  unlocksWith?: Superset;
+  stronglyRecommendedBefore?: Superset;
+  mightWatchBefore?: Superset;
+}
+
+export type Items = Array<Item> | null;
+
+export interface PhaseShow {
+  title: string;
+  genre: Genre;
+  id: string;
+  tier: Tiers;
+  unlocksWith?: Superset;
+}
+
+export type PhaseShows = Array<PhaseShow> | null;
+
+export interface Phase {
+  name: string;
+  id: number;
+  isMid: boolean;
+  unlocksWith: Superset | null;
+  items: Items;
+  phaseShows: PhaseShows;
+  development?: Items;
+  phaseShowsDevelopment?: PhaseShows;
+}
+
+export interface Group {
+  name: string;
+  id: GroupId;
+  watchBefore: string | null;
+  watchAfter: string | null;
+  items: Items;
+  development?: Items;
+}
+
+export type Phases = Array<Phase>;
+export type Groups = Array<Group>;
+
+export interface Data {
+  phases: Phases;
+  groups: Groups;
+}
